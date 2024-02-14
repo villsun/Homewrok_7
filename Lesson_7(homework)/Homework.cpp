087fb3a646dd25a770929211465ef171008bb0b7
villsun <lonukkola@gmail.com>
2024-02-14 20:52:44 +0100

    Add structure Library

---
+++ Lesson_7(homework)/Homework.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+struct Library
+{
+	char Name_Book[50];
+	char Author_Book[50];
+	char Publishing_Book[50];
+	char Genre_Book[50];
+};
+
+
+
+
+int main()
+{
+
+
+	return 0;
+}
